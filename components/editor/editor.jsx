3a523248f25f1de95db34d84aaa182ec0a84186a
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-02 09:08:51 -0500

    working

--- components/editor/editor.jsx
+++ components/editor/editor.jsx
@@ -1,11 +1,25 @@
 import UtilityBar from '../utilityBar/utilityBar'
 
+import { useContext } from 'react'
+import { MarkdownContext } from '../context/markdownContext'
+
 import classes from './editor.module.css'
 
 export default function Editor() {
+
+  const { markdown, setMarkdown } = useContext(MarkdownContext)
+
+  const handleChange = (event) => {
+    setMarkdown(event.target.value)
+  }
+
   return (
     <div className={classes.editor}>
-      <h1>Editor</h1>
+      <textarea 
+        className={classes.textarea}
+        onChange={handleChange}
+        value={markdown}
+      />
       <UtilityBar />
     </div>
   )
--- components/preview/preview.jsx
+++ components/preview/preview.jsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react'
+import { MarkdownContext } from '../context/markdownContext'
+
+import ReactMarkdown from 'react-markdown'
+
 import classes from './preview.module.css'
 
 export default function Preview() {
+
+  const { markdown } = useContext(MarkdownContext)
+
   return (
     <div className={classes.preview}>
-      <h1>Preview</h1>
+      <ReactMarkdown>
+        { markdown }
+      </ReactMarkdown>
     </div>
   )
 }
